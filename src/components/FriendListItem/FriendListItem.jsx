f3d182d12bcfc014f516c234061f4ca093926d17
D1mpson <165700584+D1mpson@users.noreply.github.com>
2026-05-01 22:52:59 +0300

    complete hw1 css

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -1,13 +1,15 @@
+import css from './FriendListItem.module.css'
+
 export default function FriendListItem({ avatar, name, isOnline }) {
   return (
-    <li>
+    <li className={css.friendListItem}>
       <img src={avatar} alt="User avatar" width="48" />
-      <p>{name}</p>
+      <p className={css.name}>{name}</p>
       <span>
         {isOnline ? (
-          <p>Online</p>
+          <p className={css.online}>Online</p>
         ) : (
-          <p>Offline</p>
+          <p className={css.offline}>Offline</p>
         )}
       </span>
     </li>
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,30 +1,33 @@
-export default function Profile ({name, tag, location, image, stats}) {
-    return (
-        <div>
-          <div>
-            <img
-              src={image}
-              alt="User avatar"
-            />
-            <p>{name}</p>
-            <p>@{tag}</p>
-            <p>{location}</p>
-          </div>
+import css from './Profile.module.css'
 
-          <ul>
-            <li>
-              <span>Followers</span>
-              <span>{stats.followers}</span>
-            </li>
-            <li>
-              <span>Views</span>
-              <span>{stats.views}</span>
-            </li>
-            <li>
-              <span>Likes</span>
-              <span>{stats.likes}</span>
-            </li>
-          </ul>
+export default function Profile({ name, tag, location, image, stats }) {
+    return (
+        <div className={css.profile}>
+            <div className={css.info}>
+                <img
+                    className={css.profileImage}
+                    src={image}
+                    alt="User avatar"
+                />
+                <p className={css.name}>{name}</p>
+                <p className={css.otherInfo}>@{tag}</p>
+                <p className={css.otherInfo}>{location}</p>
+            </div>
+
+            <ul className={css.stats}>
+                <li className={css.statsItem}>
+                    <span className={css.itemName}>Followers</span>
+                    <span className={css.itemValue}>{stats.followers}</span>
+                </li>
+                <li className={css.statsItem}>
+                    <span className={css.itemName}>Views</span>
+                    <span className={css.itemValue}>{stats.views}</span>
+                </li>
+                <li className={css.statsItem}>
+                    <span className={css.itemName}>Likes</span>
+                    <span className={css.itemValue}>{stats.likes}</span>
+                </li>
+            </ul>
         </div>
     );
 }
